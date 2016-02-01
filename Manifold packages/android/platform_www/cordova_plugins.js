5ada4d7bb12bf7c9521a19a68204d39639fecb1f
Saurabh Kirtani <sakirtan@microsoft.com>
2016-02-01 12:19:44 +0530

    Add Manifold packages

---
+++ Manifold packages/android/platform_www/cordova_plugins.js
@@ -0,0 +1,43 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-hostedwebapp/www/hostedWebApp.js",
+        "id": "cordova-plugin-hostedwebapp.hostedwebapp",
+        "pluginId": "cordova-plugin-hostedwebapp",
+        "clobbers": [
+            "hostedwebapp"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-network-information": "1.2.0",
+    "cordova-plugin-hostedwebapp": "0.2.1"
+}
+// BOTTOM OF METADATA
+});
